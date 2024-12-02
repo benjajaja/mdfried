723e446e9d2c3b2094dc39d08716f7a4931ad371
Benjamin Große <ste3ls@gmail.com>
2024-12-02 12:58:04 +0000

    update to latest ratatui-image and ratatui

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::{
 };
 
 use font_loader::system_fonts;
-use image::{GenericImage, ImageError, Rgb, RgbImage, Rgba, RgbaImage};
+use image::{GenericImage, ImageError, Pixel, Rgb, RgbImage, Rgba};
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
     layout::{Constraint, Layout, Rect},
-    style::{Modifier, Style},
+    style::{Color, Modifier, Style},
     text::{Line, Span, Text},
     widgets::{Block, Paragraph, Widget},
     DefaultTerminal, Frame,
@@ -17,74 +17,96 @@ use ratatui::{
 
 use comrak::{
     arena_tree::{Node, NodeEdge},
-    nodes::{Ast, NodeHeading, NodeValue},
+    nodes::{Ast, NodeValue},
 };
 use comrak::{parse_document, Arena, Options};
 use ratatui_image::{picker::Picker, protocol::Protocol, Image, Resize};
 use rusttype::{point, Font, Scale};
 
 fn main() -> io::Result<()> {
+    let model = Model::new().map_err::<io::Error, _>(Error::into)?;
+
     let mut terminal = ratatui::init();
     terminal.clear()?;
-    let app_result = run(terminal);
+
+    let app_result = run(terminal, model);
     ratatui::restore();
-    match app_result {
-        Err(Error::Io(io_err)) => Err(io_err),
-        Err(err) => Err(io::Error::new(io::ErrorKind::Other, format!("{err:?}"))),
-        Ok(ok) => Ok(ok),
-    }
+    app_result.map_err(Error::into)
 }
 
 struct Model<'a> {
     text: String,
-    root: Box<&'a Node<'a, RefCell<Ast>>>,
+    bg: [u8; 3],
+    scroll: i64,
+    // root: Box<&'a Node<'a, RefCell<Ast>>>,
     picker: Picker,
     font: Font<'a>,
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<(), Error> {
-    let arena = Arena::new();
+impl<'a> Model<'a> {
+    fn new() -> Result<Self, Error> {
+        let arena = Arena::new();
+        //let md = read_file_to_str("/home/gipsy/o/ratatu-image/README.md")?;
+        let text = read_file_to_str("./test.md")?;
 
-    //let md = read_file_to_str("/home/gipsy/o/ratatu-image/README.md")?;
-    let text = read_file_to_str("./test.md")?;
+        let root = parse_document(&arena, &text, &Options::default());
 
-    let root = parse_document(&arena, &text, &Options::default());
+        let mut picker = Picker::from_query_stdio()
+            .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, format!("{err}")))?;
 
-    let mut picker = Picker::from_termios()
-        .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, format!("{err}")))?;
-    picker.guess_protocol();
+        let property = system_fonts::FontPropertyBuilder::new()
+            .monospace()
+            //.family(name)
+            .family("ProFontWindows Nerd Font Mono")
+            .build();
+        let (font_data, _) =
+            system_fonts::get(&property).ok_or("Could not get system fonts property")?;
 
-    let property = system_fonts::FontPropertyBuilder::new()
-        .monospace()
-        //.family(name)
-        .family("ProFontWindows Nerd Font Mono")
-        .build();
-    let (font_data, _) =
-        system_fonts::get(&property).ok_or("Could not get system fonts property")?;
+        let font = Font::try_from_vec(font_data).ok_or(Error::NoFont)?;
 
-    let font = Font::try_from_vec(font_data).ok_or(Error::NoFont)?;
+        let bg = [0, 0, 50];
+        picker.set_background_color(Some(image::Rgb(bg)));
 
-    let mut model = Model {
-        text,
-        root: Box::new(root),
-        picker,
-        font,
-    };
+        Ok(Model {
+            text,
+            bg,
+            scroll: 0,
+            // root: Box::new(root),
+            picker,
+            font,
+        })
+    }
+}
 
+fn run(mut terminal: DefaultTerminal, mut model: Model) -> Result<(), Error> {
     loop {
         terminal.draw(|frame| view(&mut model, frame))?;
 
         if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') => {
+                        return Ok(());
+                    }
+                    KeyCode::Char('j') => {
+                        model.scroll += 1;
+                    }
+                    KeyCode::Char('k') => {
+                        model.scroll -= 1;
+                    }
+                    _ => {}
+                }
             }
         }
     }
 }
 
-fn view(model: &mut Model, f: &mut Frame) {
-    let [left, right] =
-        Layout::horizontal([Constraint::Fill(2), Constraint::Fill(1)]).areas(f.area());
+fn view(model: &mut Model, frame: &mut Frame) {
+    let area = frame.area();
+    let block = Block::default()
+        .title("Custom Background Block")
+        .style(Style::default().bg(Color::Rgb(model.bg[0], model.bg[1], model.bg[2])));
+    frame.render_widget(block, area);
 
     let mut debug = vec![];
     let mut lines = vec![];
@@ -94,7 +116,10 @@ fn view(model: &mut Model, f: &mut Frame) {
     let mut y = 0;
     //let mut span = None;
 
-    for edge in model.root.traverse() {
+    let arena = Arena::new();
+    let root = parse_document(&arena, &model.text, &Options::default());
+
+    for edge in root.traverse() {
         match edge {
             NodeEdge::Start(node) => match node.data.borrow().value {
                 ref node_value => {
@@ -111,19 +136,25 @@ fn view(model: &mut Model, f: &mut Frame) {
                         spans.push(span);
                     }
                     NodeValue::Heading(ref tier) => {
-                        let widget =
-                            Header::new(&mut model.picker, &mut model.font, left.width, spans)
-                                .unwrap();
-                        let height = widget.proto.rect().height;
-                        y = render_lines(widget, height, y, left, f);
+                        let widget = Header::new(
+                            &mut model.picker,
+                            &mut model.font,
+                            model.bg,
+                            area.width,
+                            spans,
+                            tier.level,
+                        )
+                        .unwrap();
+                        let height = widget.height;
+                        y = render_lines(widget, height, y, area, frame);
                         lines = vec![];
                         spans = vec![];
                     }
                     NodeValue::Image(ref link) => {
                         let widget =
-                            LinkImage::new(&mut model.picker, left.width, link.url.as_str());
-                        let height = widget.proto.rect().height;
-                        y = render_lines(widget, height, y, left, f);
+                            LinkImage::new(&mut model.picker, area.width, link.url.as_str());
+                        let height = widget.height;
+                        y = render_lines(widget, height, y, area, frame);
                         lines = vec![];
                         spans = vec![];
                     }
@@ -135,7 +166,7 @@ fn view(model: &mut Model, f: &mut Frame) {
                         spans = vec![];
                         let height = text.height() as u16;
                         let p = Paragraph::new(text);
-                        y = render_lines(p, height, y, left, f);
+                        y = render_lines(p, height, y, area, frame);
                     }
                     NodeValue::LineBreak | NodeValue::SoftBreak => {
                         lines.push(Line::from(spans));
@@ -144,7 +175,7 @@ fn view(model: &mut Model, f: &mut Frame) {
                         spans = vec![];
                         let height = text.height() as u16;
                         let p = Paragraph::new(text);
-                        y = render_lines(p, height, y, left, f);
+                        y = render_lines(p, height, y, area, frame);
                     }
                     _ => {
                         if let CookedModifier::Raw(modifier) = modifier(&node.data.borrow().value) {
@@ -155,29 +186,34 @@ fn view(model: &mut Model, f: &mut Frame) {
             }
         }
     }
-
-    f.render_widget(Paragraph::new(debug).block(Block::bordered()), right);
 }
 
 struct Header {
-    proto: Box<dyn Protocol>,
+    proto: Protocol,
+    height: u16,
 }
 
 impl<'a> Header {
     fn new(
         picker: &mut Picker,
         font: &mut Font<'a>,
+        bg: [u8; 3],
         width: u16,
         spans: Vec<Span>,
+        tier: u8,
     ) -> Result<Header, Error> {
         let cell_height = 2;
-        let img_width = (width * picker.font_size.0) as u32;
-        let img_height = (cell_height * picker.font_size.1) as u32;
-        let img: RgbImage = RgbImage::from_pixel(img_width, img_height, Rgb([0, 0, 0]));
+        let (font_width, font_height) = picker.font_size();
+        let img_width = (width * font_width) as u32;
+        let img_height = (cell_height * font_height) as u32;
+        let img: RgbImage = RgbImage::from_pixel(img_width, img_height, Rgb(bg));
         let mut dyn_img = image::DynamicImage::ImageRgb8(img);
 
+        //let mut spans = spans.clone();
+        //spans.push(Span::raw(format!("#{tier}")));
         let s: String = spans.iter().map(|s| s.to_string()).collect();
-        let scale = Scale::uniform(64f32);
+        let tier_scale = ((12 - tier) as f32) / 12.0f32;
+        let scale = Scale::uniform((font_height * cell_height) as f32 * tier_scale);
         let v_metrics = font.v_metrics(scale);
         let glyphs: Vec<_> = font
             .layout(&s, scale, point(0.0, 0.0 + v_metrics.ascent))
@@ -199,7 +235,8 @@ impl<'a> Header {
                         outside = true;
                     } else {
                         let u8v = (255.0 * v) as u8;
-                        let pixel = Rgba([u8v, u8v, u8v, 255]);
+                        let mut pixel = Rgba([bg[0], bg[1], bg[2], 255]);
+                        pixel.blend(&Rgba([u8v, u8v, u8v, u8v]));
                         dyn_img.put_pixel(p_x, p_y, pixel);
                     }
                 });
@@ -216,7 +253,10 @@ impl<'a> Header {
                 Resize::Fit(None),
             )
             .unwrap();
-        Ok(Header { proto })
+        Ok(Header {
+            proto,
+            height: cell_height,
+        })
     }
 }
 
@@ -225,23 +265,25 @@ impl Widget for Header {
     where
         Self: Sized,
     {
-        let image = Image::new(self.proto.as_ref());
+        let image = Image::new(&self.proto);
         image.render(area, buf);
     }
 }
 
 struct LinkImage {
-    proto: Box<dyn Protocol>,
+    proto: Protocol,
+    height: u16,
 }
 
 impl LinkImage {
     fn new(picker: &mut Picker, width: u16, link: &str) -> LinkImage {
-        let dyn_img = image::io::Reader::open(link).unwrap().decode().unwrap();
+        let dyn_img = image::ImageReader::open(link).unwrap().decode().unwrap();
+        let height: u16 = 10;
 
         let proto = picker
-            .new_protocol(dyn_img, Rect::new(0, 0, width, width), Resize::Fit(None))
+            .new_protocol(dyn_img, Rect::new(0, 0, width, height), Resize::Fit(None))
             .unwrap();
-        LinkImage { proto }
+        LinkImage { proto, height }
     }
 }
 
@@ -250,7 +292,7 @@ impl Widget for LinkImage {
     where
         Self: Sized,
     {
-        let image = Image::new(self.proto.as_ref());
+        let image = Image::new(&self.proto);
         image.render(area, buf);
     }
 }
@@ -261,6 +303,15 @@ enum Error {
     Image(image::ImageError),
     Msg(String),
     NoFont,
+}
+
+impl Into<io::Error> for Error {
+    fn into(self) -> io::Error {
+        match self {
+            Error::Io(io_err) => io_err,
+            err => io::Error::new(io::ErrorKind::Other, format!("{err:?}")),
+        }
+    }
 }
 
 impl From<&str> for Error {
@@ -282,10 +333,7 @@ impl From<std::io::Error> for Error {
 }
 
 fn render_lines<W: Widget>(widget: W, height: u16, y: u16, area: Rect, f: &mut Frame) -> u16 {
-    //let text = Text::from(lines);
-    //let next_y = y + height;
     if y < area.height && area.height - y > height {
-        //let p = Paragraph::new(text);
         let mut area = area.clone();
         area.y += y;
         area.height = height;
