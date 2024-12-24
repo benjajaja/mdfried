87917214e7512b6f3f91f7b95002c54ca35393a8
Benjamin Große <ste3ls@gmail.com>
2024-12-24 14:01:11 +0000

    separate deps
    but still needs nativeBuildInputs in buildInputs, something's wrong

--- flake.nix
+++ flake.nix
@@ -9,18 +9,39 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
         craneLib = crane.mkLib pkgs;
-      in
-    {
-      packages.default = craneLib.buildPackage {
         src = craneLib.cleanCargoSource ./.;
 
-        # Add extra inputs here or any other derivation settings
-        doCheck = true;
-        nativeBuildInputs = with pkgs; [
-          cmake pkg-config freetype expat fontconfig
-        ];
-        buildInputs = with pkgs; [];
-      };
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          nativeBuildInputs = with pkgs; [
+            cmake pkg-config freetype expat fontconfig
+          ];
+          buildInputs = with pkgs; [
+            # Add additional build inputs here
+            cmake pkg-config freetype expat fontconfig
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        mdfried = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          doCheck = false;
+        });
+      in
+    {
+      packages.default = mdfried;
     });
 }
