47123d2d29f992516a9ce12d19956129c4260a44
Benjamin Große <ste3ls@gmail.com>
2025-01-29 09:49:12 +0000

    Reapply "use rust-fontconfig"
    This reverts commit de84b6fd765ad05e6e8416ffa681dc4acce0163f.

--- src/setup.rs
+++ src/setup.rs
@@ -1,12 +1,13 @@
-use font_loader::system_fonts;
+use std::{collections::BTreeMap, fs::File, io::Read};
 
 use ratatui_image::{
     picker::{Picker, ProtocolType},
     FontSize,
 };
+use rust_fontconfig::{FcFontCache, FcFontPath};
 use rusttype::Font;
 
-use crate::{config::Config, error::Error, fontpicker::set_up_font, CONFIG};
+use crate::{config::Config, error::Error, fontpicker::interactive_font_picker, CONFIG};
 
 pub struct Renderer<'a> {
     pub picker: Picker,
@@ -43,25 +44,30 @@ pub fn setup_graphics<'a>(
         }
     };
 
-    let mut fp_builder = system_fonts::FontPropertyBuilder::new();
+    let cache = FcFontCache::build();
 
-    let all_fonts = system_fonts::query_all();
+    let all_font_families: BTreeMap<String, &FcFontPath> = cache
+        .list()
+        .iter()
+        .filter_map(|(pattern, path)| pattern.family.clone().map(|family| (family, path)))
+        .collect();
 
     let config_font_family = font_family.and_then(|font_family| {
         // Ensure this font exists
-        if all_fonts.contains(&font_family) {
+        if all_font_families.contains_key(&font_family) {
             return Some(font_family);
         }
+        println!("Configured font not found: {font_family}");
         None
     });
 
     let font_family = if let Some(mut font_family) = config_font_family {
         if force_font_setup {
             println!("Entering forced font setup");
-            match set_up_font(&mut picker, bg) {
+            match interactive_font_picker(&cache, &mut picker, bg) {
                 Ok(Some(setup_font_family)) => {
                     let new_config = Config {
-                        font_family: Some(font_family.clone()),
+                        font_family: Some(setup_font_family.clone()),
                         ..Default::default()
                     };
                     confy::store(CONFIG.0, CONFIG.1, new_config)?;
@@ -74,7 +80,7 @@ pub fn setup_graphics<'a>(
         font_family
     } else {
         println!("Entering one-time font setup");
-        match set_up_font(&mut picker, bg) {
+        match interactive_font_picker(&cache, &mut picker, bg) {
             Ok(Some(font_family)) => {
                 let new_config = Config {
                     font_family: Some(font_family.clone()),
@@ -88,12 +94,16 @@ pub fn setup_graphics<'a>(
         }
     };
 
-    fp_builder = fp_builder.family(&font_family);
+    let result = all_font_families.get(&font_family).ok_or(Error::NoFont)?;
 
-    let property = fp_builder.build();
+    let font = load_font(&result.path)?;
 
-    let (font_data, _) = system_fonts::get(&property).ok_or(Error::NoFont)?;
-
-    let font = Font::try_from_vec(font_data).ok_or(Error::NoFont)?;
     Ok(Some(Renderer::new(picker, font, bg)))
+}
+
+pub fn load_font<'a>(path: &str) -> Result<Font<'a>, Error> {
+    let mut file = File::open(path)?;
+    let mut contents = vec![];
+    file.read_to_end(&mut contents)?;
+    Font::try_from_vec(contents).ok_or(Error::NoFont)
 }
